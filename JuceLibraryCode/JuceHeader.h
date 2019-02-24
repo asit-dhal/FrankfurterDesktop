1bda865fd40778cb1e37065ef43caa98b8499e4a
Asit Dhal <dhal.asitk@gmail.com>
2019-02-24 03:04:03 +0100

    appveyor ci

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -15,7 +15,6 @@
 #include "AppConfig.h"
 
 #include <juce_core/juce_core.h>
-#include <juce_cryptography/juce_cryptography.h>
 #include <juce_data_structures/juce_data_structures.h>
 #include <juce_events/juce_events.h>
 #include <juce_graphics/juce_graphics.h>
